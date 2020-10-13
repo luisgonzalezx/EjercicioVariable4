7a0aee4cb6d5c556b38824db0edd2cbaf436986c
luis <lagonzalez1688@gmail.com>
2020-10-13 00:39:56 -0700

    Ejercicio variable 4

---
+++ src/ejerciciovariable4/Ejerciciovariable4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciovariable4;
+
+
+public class Ejerciciovariable4 {
+
+    
+    public static void main(String[] args) {
+        
+         int a=6;
+        
+          if(a%2!=0) 
+        
+            System.out.print("es numero impar");
+          
+          else
+              System.out.print("es numero par");
+        
+    }
+    
+}
